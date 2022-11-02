a9943f49e36a1fa2bc390f8395d33e4234d84c88
Leonardo Tomio <leo__tomio@hotmail.com>
2022-11-02 20:32:57 -0300

    0.0.1 -> crianção do projeto

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header>
+            <h1>Teste Header</h1>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/pages/Filme/index.js
@@ -0,0 +1,8 @@
+function Filme() {
+    return (
+        <div>
+            <h1>Bem vindo a pagina Detalhes do Filme</h1>
+        </div>
+    )
+}
+export default Filme;
