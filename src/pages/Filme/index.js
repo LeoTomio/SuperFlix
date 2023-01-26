a120af198a88156ee9e14fd082d446e558156fb1
Leonardo Tomio <leo__tomio@hotmail.com>
2023-01-25 21:54:52 -0300

    0.0.4 -> botando estilo no detalamento do filme

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,7 +1,62 @@
+import { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+
+import api from '../../services/api';
+import './filme-info.css'
 function Filme() {
+    const { id_filme } = useParams();
+    const navigate = useNavigate()
+    const [filme, setFilme] = useState({})
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        async function loadFilme() {
+            await api.get(`/movie/${id_filme}`, {
+                params: {
+                    api_key: 'b0e0b492cd887aede31ca030d554b60b',
+                    language: 'pt-BR',
+                }
+
+            }).then((response) => {
+                setFilme(response.data)
+                setLoading(false)
+            }).catch(() => {
+                navigate("/", { replace: true })
+                console.log('filme nao encontrado')
+            })
+        }
+        loadFilme();
+
+
+        return () => {
+            console.log('COMPONENTE DESMONTADO')
+        }
+    }, [])
+
+    if (loading) {
+        return (
+            <div className='filme-info'>
+                <h1>Carregando detalhes...</h1>
+            </div>
+        )
+    }
+
+
     return (
-        <div>
-            <h1>Bem vindo a pagina Detalhes do Filme</h1>
+        <div className='filme-info'>
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+            <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+            <div className='area-buttons'>
+                <button>Salvar</button>
+                <button>
+                    <a href='#'>Trailer</a>
+                </button>
+            </div>
+
         </div>
     )
 }
