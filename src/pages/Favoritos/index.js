d7a8516a3410b0ab680d2c6d9641418fed9b641e
Leonardo Tomio <leo__tomio@hotmail.com>
2023-01-26 21:35:42 -0300

    0.2.0 -> Função de excluir filmes dos favoritos + Alerts personalizados

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import './favoritos.css'
 import { Link } from 'react-router-dom';
-
+import { toast } from 'react-toastify'
 function Favoritos() {
 
     const [filmes, setFilmes] = useState([]);
@@ -11,11 +11,23 @@ function Favoritos() {
         setFilmes(JSON.parse(minhaLista) || [])
     }, [])
 
+    function excluirItens(id) {
+        let filmesFiltrados = filmes.filter((item) => {
+            return (item.id !== id)
+        })
+
+        setFilmes(filmesFiltrados)
+        localStorage.setItem('@primeflix', JSON.stringify(filmesFiltrados))
+        toast.success('Filme removido com sucesso!!')
+    }
 
 
     return (
         <div className='meus-filmes'>
             <h1>Meus filmes</h1>
+
+            {filmes.length === 0 && <span> Você não possui nenhum filme na lista :(</span>}
+
             <ul>
                 {filmes.map((item) => {
                     return (
@@ -23,7 +35,7 @@ function Favoritos() {
                             <span>{item.title}</span>
                             <div>
                                 <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
-                                <button>Excluir</button>
+                                <button onClick={() => { excluirItens(item.id) }}>Excluir</button>
                             </div>
                         </li>
                     )
--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 
+import { toast } from 'react-toastify'
 import api from '../../services/api';
 import './filme-info.css'
 function Filme() {
@@ -49,12 +50,12 @@ function Filme() {
         const hasFilme = filmesSalvos.some((item) => item.id === filme.id)
 
         if (hasFilme) {
-            alert('Filme já esta na sua lista');
+            toast.warn('Filme já esta na sua lista');
             return;
         }
         filmesSalvos.push(filme);
         localStorage.setItem('@primeflix', JSON.stringify(filmesSalvos));
-        alert("Filme salvo com sucesso")
+        toast.success("Filme salvo com sucesso")
 
 
     }
