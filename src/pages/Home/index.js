8b49feeaababc55e2dc5fa149e8d8e4097b2b69b
Leo Tomio <leo__tomio@hotmail.com>
2024-11-08 13:18:41 -0300

    ajustes na listagem

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -19,7 +19,7 @@ function Home() {
                     language: 'pt-BR',
                     page: 1
                 }
-                
+
             })
             //console.log(response.data.results.slice(0,10));
             setFilmes(response.data.results.slice(0, 10))
@@ -29,8 +29,8 @@ function Home() {
         loadFilmes()
     })
 
-    if(loading){
-        return(
+    if (loading) {
+        return (
             <div className="loading">
                 <h2>Carregando Filmes...</h2>
 
@@ -42,18 +42,19 @@ function Home() {
     return (
         <div className='container'>
             <div className='lista-filmes'>
-                {filmes.map((filme) => {
-                    return (
-                        <article key={filme.id}>
-                            <strong>{filme.title}</strong>
+                {filmes.map((filme) => (
+                    <article key={filme.id}>
+                        <strong>{filme.title}</strong>
+                        <div className="imagem-e-botao">
                             <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
-                            <Link to={`/filme/${filme.id}`}> Acessar </Link>
-                        </article>
-                    )
-                })}
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </div>
+                    </article>
+                ))}
                 <h1>Bem vindo a Home</h1>
             </div>
         </div>
+
     )
 }
 export default Home;
